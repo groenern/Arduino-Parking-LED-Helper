bb09b228d4decdb26695e84ceb2ae5bdb96b176d
groenern <130081417+groenern@users.noreply.github.com>
2023-05-15 07:56:40 -0500

    Update Ping_Parking_Assistant.ino
    Changed Arduino Libraries and fixed some logic Issues. Upgraded to a TFLuna Lidar device.

--- Ping_Parking_Assistant.ino
+++ Ping_Parking_Assistant.ino
@@ -1,50 +1,65 @@
+#include <Arduino.h>
+#include <Wire.h>        // Instantiate the Wire library
+#include <TFLI2C.h>      // TFLuna-I2C Library v.0.1.1
 #include <FastLED.h>
 
 #define NUM_LEDS 60
-#define LED_PIN 3 
-#define PING_PIN 7
+#define LED_PIN 3
 
+TFLI2C tflI2C; // Object for lidar sensor
+
+int16_t  tfDist;    // distance in centimeters
+int16_t  tfAddr = TFL_DEF_ADR;  // Use this default I2C address
+
+// LED Essentials
 CRGB leds[NUM_LEDS];
+const int ledMaxDistance = 200; 
+const int ledMinDistance = 30; 
+const int flashDelay = 200; // Delay between red blinking
+const int numFlashes = 75; // Number of flashes before LED strip turns off
 
-const double ledMinDistance = 10; // distances are in cm
-const double ledMaxDistance = 100; 
-const int flashDelay = 100; // delay in ms
-
-void setup() {
-  Serial.begin(9600);
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  fill_solid(leds, NUM_LEDS, CRGB::Black); // Turn off all LEDs
-  FastLED.show(); // Show the updated LED states
+// Returns Distance from Sensor in CM
+int16_t getDistance() {
+  if(tflI2C.getData(tfDist, tfAddr)) {
+    return tfDist;
+  }
 }
 
-void loop() {
-  long duration, inches, cm;
-  int loopCounter = 0, numLeds = 0;
+// Setup Sensor And LED
+void setup(){
+  // Serial.begin(9600); DEBUGGING
+  Wire.begin();           // Initalize Wire library
+  
+  // Add FASTLED and Set all Lights to blank
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  fill_solid(leds, NUM_LEDS, CRGB::Black); 
+  FastLED.show();
+}
 
-  pinMode(PING_PIN, OUTPUT);
-  digitalWrite(PING_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(PING_PIN, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(PING_PIN, LOW);
+void loop(){
+  // Variables used for calculations
+  int16_t distance;
+  int numLeds;
+  int loopCounter = 0;
 
-  pinMode(PING_PIN, INPUT);
-  duration = pulseIn(PING_PIN, HIGH);
+  distance = getDistance();
+  // Serial.println(String(distance) + " cm"); DEBUGGING
 
-  inches = microsecondsToInches(duration);
-  cm = microsecondsToCentimeters(duration);
+  if (distance >= ledMaxDistance ){ // If the distance reading is out of max led range... turn off all lights 
+    fill_solid(leds, NUM_LEDS, CRGB::Black); 
+  } else if (distance > ledMinDistance && distance < ledMaxDistance){ // Distance is inbetween min and max.. calculate numLeds and light up based on distance
+    fill_solid(leds, NUM_LEDS, CRGB::Black); 
+  
+    numLeds = distance * NUM_LEDS / ledMaxDistance;
 
-  /* DEBUGGING
-  Serial.print("Distance: ");
-  Serial.print(cm);
-  Serial.println(" cm");
-  */
-
-  if (cm >= ledMaxDistance) {
-    fill_solid(leds, NUM_LEDS, CRGB::Black); // out of range... nothing lit up 
-  } else if (cm <= ledMinDistance) { // flash red on and off 
-    while(cm <= ledMinDistance) { 
-      if (loopCounter < 100) { 
+    if (distance >= ledMaxDistance / 2){ // half is green, half is orange to indicate getting closer
+      fill_solid(leds, numLeds, CRGB::Green);
+    } else {
+      fill_solid(leds, numLeds, CRGB::Orange);
+    }
+  } else if (distance <= ledMinDistance){ // Distance is below max... flash red light for a little... then turn off
+    while (distance < ledMinDistance){
+       if (loopCounter < numFlashes) { 
         fill_solid(leds, NUM_LEDS, CRGB::Red);
         FastLED.show();
         delay(flashDelay);
@@ -56,30 +71,13 @@ void loop() {
       } else {
         fill_solid(leds, NUM_LEDS, CRGB::Black);
         FastLED.show();
+        delay(10000); // Delay to Check Only every 10 Seconds 
       }
-    }
 
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-  } else if (cm > ledMinDistance && cm < ledMaxDistance){ // calculate numLeds and light up based on distance
-    fill_solid(leds, NUM_LEDS, CRGB::Black); 
-  
-    numLeds = cm * NUM_LEDS / ledMaxDistance;
-
-    if (cm >= ledMaxDistance/2){ // half is green, half is orange to indicate getting closer
-      fill_solid(leds, numLeds, CRGB::Green);
-    } else {
-      fill_solid(leds, numLeds, CRGB::Orange);
+      distance = getDistance(); // Update distance sensor during while loop
     }
   }
 
-  FastLED.show(); // Show the updated LED states
-  delay(100);
-}
-
-long microsecondsToInches(long microseconds) {
-  return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds) {
-  return microseconds / 29 / 2;
+  FastLED.show();
+  delay(100); // reduces jiterry LEDS while remaining at a stable distance
 }
